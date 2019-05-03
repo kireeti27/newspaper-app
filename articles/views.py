6986653272399984037d0b4e806ddfd0969350e1
kireeti <revurikireeti@gmail.com>
2019-05-03 23:48:30 +0530

    newspaper app

---
+++ articles/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.views.generic import *
+from .models import Article
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+   
+class ArticleCreateView(LoginRequiredMixin,CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title','body',)
+    login_url = 'login'
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ArticleDetailView(LoginRequiredMixin,DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+    login_url = 'login'
+
+class ArticleUpdateView(LoginRequiredMixin,UpdateView):
+    model = Article
+    template_name = 'article_edit.html'
+    fields = ('title','body')
+    login_url = 'login'
+
+    def dispatch(self, request, *args, **kwargs): # new
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+    
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+    login_url = 'login'
+
+    def dispatch(self, request, *args, **kwargs): # new
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
